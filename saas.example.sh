6d5744c0fb7cde4b1c694da55f6d6c15c1328ac1
Liu Xiaoyi <circuitcoder0@gmail.com>
2021-08-11 23:22:23 +0800

    Provide accept function

--- saas.example.sh
+++ saas.example.sh
@@ -15,7 +15,8 @@ echo "Sample SHA256 calculation" > $PIPENAME &
 sha256sum < $PIPENAME
 
 echo "--------"
-echo "Try: curl http://localhost:1080"
+echo "Try: curl -I http://localhost:1080"
+echo "Also try: wget http://localhost:1080"
 echo "Use Ctrl-C to terminate the server"
 echo "--------"
 
@@ -25,14 +26,25 @@ echo "--------"
 #
 ###############################################
 
+#
+# TODO: change to SHA256 as a Service server implementation
+#
+function accept {
+  read -r INPUT
+  >&2 echo "Got request, first line: $INPUT"
+
+  echo -e "HTTP/1.1 404 Not Found\r\n\r" 
+}
+
 while true; do
 
   # Warning for Mac / Windows users:
   # The `-N` option for netcat is used to terminate connections on EOF.
   # It may have other names on your distro / OS's netcat implementation (especially if you are a mac user)
   # Check `man nc` for detail.
-  #
-  # TODO: change to SHA256 as a Service server implementation
-  echo -e "HTTP/1.1 404 Not Found\r\n\r" | nc -N -l 1080
-  echo "--------"
+  FIFONAME=$(mktemp -u)
+  mkfifo $FIFONAME
+
+  # Named pipe loop
+  nc -N -l 1080 < $FIFONAME | accept > $FIFONAME
 done
